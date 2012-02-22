0ac2af62d480ee1f765015a04650b2ab09e40c59
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-22 18:57:13 +0100

    Model & View created
    A build model and view is created.
    
    Note: currently it is serving hardcoded builds.

--- js/candybar.js
+++ js/candybar.js
@@ -1,5 +1,32 @@
 (function($, undefined){
+    var BuildModel = Backbone.Model.extend({});
+
+    var Builds = Backbone.Collection.extend({
+	model: BuildModel
+    });
+
+    var BuildsView = Backbone.View.extend({
+	template: _.template("<tr><td><%= number %></td><td><%= commit %></td><td><%= status %></td></tr>"),
+
+	initialize: function(){
+	    _.bindAll(this, 'render');
+
+	    this.render();
+	},
+
+	render: function(){
+	    var element = $(this.el), template = this.template;
+	    element.empty();
+	    this.model.each(function(model){
+		element.append(template(model.toJSON()));
+	    });
+	}
+    });
+
     $(function(){
-	console.log("candybar initialized");
+	var builds = new Builds();
+	builds.add({number: 37, commit: 51, status: 0});
+	builds.add({number: 34, commit: 35, status: 0});
+	var buildsView = new BuildsView({el: $("#builds"), model: builds});
     });
 })( jQuery );
