5b07e102b60b1a03c202dcd5ab92acc0abf11cd2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-22 21:26:27 +0100

    Data fetched
    Data is fetched from the (local) server.
    
    Note: We had cross domain script problems. Signal Travis to use
    jsonp.

--- js/candybar.js
+++ js/candybar.js
@@ -2,11 +2,27 @@
     var BuildModel = Backbone.Model.extend({});
 
     var Builds = Backbone.Collection.extend({
-	model: BuildModel
+	model: BuildModel,
+
+	sync: function(method, model){
+	    if (method == 'read') {
+		$.ajax({
+		    url: "data/static.json",
+		    dataType: "json",
+		    success: function(data){
+			_.each(data, function(build){
+			    console.log(build);
+			    model.add(build);
+			});
+		    },
+		    async: false
+		});
+	    }
+	}
     });
 
     var BuildsView = Backbone.View.extend({
-	template: _.template("<tr><td><%= number %></td><td><%= commit %></td><td><%= status %></td></tr>"),
+	template: _.template("<tr><td><%= number %></td><td><%= commit %></td><td><%= result %></td></tr>"),
 
 	initialize: function(){
 	    _.bindAll(this, 'render');
@@ -25,8 +41,7 @@
 
     $(function(){
 	var builds = new Builds();
-	builds.add({number: 37, commit: 51, status: 0});
-	builds.add({number: 34, commit: 35, status: 0});
+	builds.fetch();
 	var buildsView = new BuildsView({el: $("#builds"), model: builds});
     });
 })( jQuery );
